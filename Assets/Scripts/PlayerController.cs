aab3c6f662dfc63137e400ad49ce170d56dd153f
Farid <farid.teresniak@hotmail.de>
2022-11-11 07:50:24 +0100

    Started PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,17 +10,26 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float movementSpeed = 5f;
     [SerializeField] float jumpPower = 5f;
+    [SerializeField] int maxJumps = 2;
+    int currentJumps;
 
 
     Rigidbody2D rb;
 
     float horizontalInput;
-    bool startFloating;
 
-    Vector2 defaultGravity;
-    [SerializeField] Vector2 floatingGravity;
-    Vector2 currentGravity;
+    bool isFloating;
+    float defaultGravityScale;
+    [SerializeField] float floatingGravityScale = 0.2f;
+    [SerializeField] float floatingTime = 3f;
+    float floatTimer = 0f;
+    [SerializeField] AnimationCurve floatingCurve;
 
+    [SerializeField] Transform groundCheckPosition;
+    [SerializeField] float groundCheckRadius = 0.1f;
+    [SerializeField] LayerMask groundCheckLayers;
+
+    bool isGrounded => Physics2D.OverlapCircle(new Vector2(transform.position.x,transform.position.y - 0.5f), groundCheckRadius, groundCheckLayers); 
 
     #endregion
 
@@ -29,13 +39,25 @@ public class PlayerController : MonoBehaviour
     {
         // Initialize components.
         rb = GetComponent<Rigidbody2D>();
-        currentGravity = defaultGravity;
+        defaultGravityScale = rb.gravityScale;
+        maxJumps--;
+        currentJumps = maxJumps;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentJumps = isGrounded ? maxJumps : currentJumps;
+
+        // Handle floating mechanic.
+        if (isFloating)
+        {
+            floatTimer += Time.deltaTime;
+            if (floatTimer >= floatingTime)
+            {
+                StopFloating();
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -53,20 +75,66 @@ public class PlayerController : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext ctx)
     {
-        //horizontalInput = ctx.ReadValue<float>();
+        if (ctx.performed && currentJumps > 0)
+        {
+            Jump();
+        }
     }
+
+    
 
     public void Float(InputAction.CallbackContext ctx)
     {
-        if (ctx.phase == InputActionPhase.Performed)
+        if (ctx.performed)
         {
-            startFloating = true;
+            StartFloating();
         }
-        else if (ctx.phase == InputActionPhase.Canceled)
+        else if (ctx.canceled)
         {
-            startFloating = false;
+            StopFloating();
         }
         
     }
     #endregion
+
+    #region UtilityMethods
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        
+
+        if (isFloating)
+        {
+            rb.AddForce(Vector2.up * jumpPower * floatingGravityScale, ForceMode2D.Impulse);
+        }
+        else
+        {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+
+        currentJumps--;
+        // ToDo: Enable particles and sound.
+    }
+
+    private void StartFloating()
+    {
+        isFloating = true;
+        rb.gravityScale = floatingGravityScale;
+
+        // ToDo: Enable floating particles and sound. Modify horizontal movement speed.
+    }
+    private void StopFloating()
+    {
+        isFloating = false;
+        floatTimer = 0f;
+        rb.gravityScale = defaultGravityScale;
+    }
+
+    #endregion
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(new Vector2(transform.position.x, transform.position.y - 0.5f), groundCheckRadius);
+    }
 }
