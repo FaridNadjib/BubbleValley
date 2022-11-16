1b97ef347fae2acff7f4f3c8c4d3af50642c9e3f
FaridNadjib <64439155+FaridNadjib@users.noreply.github.com>
2022-11-16 15:59:18 +0100

    PlayerController changed.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,34 +8,44 @@ public class PlayerController : MonoBehaviour
 {
     #region Fields
 
+    [Header("Basic movement related:")]
     [SerializeField] float movementSpeed = 5f;
     [SerializeField] float jumpPower = 5f;
     [SerializeField] int maxJumps = 2;
     int currentJumps;
 
-
     Rigidbody2D rb;
 
-    float horizontalInput;
-
-    bool isFloating;
-    float defaultGravityScale;
+    [Header("Floating related:")]
     [SerializeField] float floatingGravityScale = 0.2f;
     [SerializeField] float floatingTime = 3f;
+    bool isFloating;
+    float defaultGravityScale;
     float floatTimer = 0f;
-    [SerializeField] AnimationCurve floatingCurve;
 
-    [SerializeField] GameObject floatingEfector;
-
+    [Header("Ground check related:")]
     [SerializeField] Transform groundCheckPosition;
     [SerializeField] float groundCheckRadius = 0.1f;
     [SerializeField] LayerMask groundCheckLayers;
-
     bool isGrounded => Physics2D.OverlapCircle(new Vector2(transform.position.x,transform.position.y - 0.5f), groundCheckRadius, groundCheckLayers);
 
+    [Header("Power charge related:")]
+    [SerializeField] float powerChargePowerMultiplier = 5f;
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] float maxChargeTime = 3f;
+    bool isPowerCharging;
+    float chargeTimer;
+    
 
+    
+    [SerializeField] AnimationCurve floatingCurve;
+
+    // Input action asset.
     PlayerInput playerInput;
     InputAction movementInput;
+    float startTime = 0;
+
+
     #endregion
 
 
@@ -45,12 +55,14 @@ public class PlayerController : MonoBehaviour
         // Initialize components.
         rb = GetComponent<Rigidbody2D>();
         defaultGravityScale = rb.gravityScale;
+
+        // Todo: fix maxjumps.
         maxJumps--;
         currentJumps = maxJumps;
 
-
+        // Input action related.
         playerInput = GetComponent<PlayerInput>();
-        movementInput = playerInput.actions.FindAction("Move");
+        movementInput = playerInput.actions.FindAction("MovementInput");
         movementInput.Enable();
 
     }
@@ -58,6 +70,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Reset current jumps if the character touches the ground.
         currentJumps = isGrounded ? maxJumps : currentJumps;
 
         // Handle floating mechanic.
@@ -67,28 +80,33 @@ public class PlayerController : MonoBehaviour
             if (floatTimer >= floatingTime)
             {
                 StopFloating();
-            }
-
-            floatingEfector.transform.position = transform.position;
-            floatingEfector.SetActive(true);
+            }            
         }
 
-        Debug.Log(movementInput.ReadValue<Vector2>());
+        //Debug.Log(movementInput.ReadValue<Vector2>());
+
+        // Handle power charge.
+        if (isPowerCharging)
+        {
+            if (chargeTimer != 0)
+            {
+                rb.AddForce(Vector2.up * chargeTimer / maxChargeTime * powerChargePowerMultiplier);
+                isPowerCharging = false;
+                chargeTimer = 0f;
+            }
+            
+        }
     }
 
     private void FixedUpdate()
     {
+
         // Handle player movement.
         rb.AddForce(movementInput.ReadValue<Vector2>().x * movementSpeed * Vector2.right);
     }
 
 
     #region InputMethods
-    public void HorizontalInput(InputAction.CallbackContext ctx)
-    {
-        horizontalInput = ctx.ReadValue<float>();
-    }
-
     public void Jump(InputAction.CallbackContext ctx)
     {
         if (ctx.performed && currentJumps > 0)
@@ -96,8 +114,6 @@ public class PlayerController : MonoBehaviour
             Jump();
         }
     }
-
-    
 
     public void Float(InputAction.CallbackContext ctx)
     {
@@ -111,22 +127,40 @@ public class PlayerController : MonoBehaviour
         }
         
     }
+
+
+    public void PowerCharge(InputAction.CallbackContext ctx)
+    {
+        
+        if (ctx.performed)
+        {
+            isPowerCharging = true;
+            startTime = (float)ctx.time;
+        }
+        else if (ctx.canceled)
+        {
+            //isPowerCharging = false;
+            chargeTimer = (float)(ctx.time - startTime);
+            Math.Clamp(chargeTimer, 0, maxChargeTime);
+            Debug.Log(chargeTimer);
+        }
+    }      
     #endregion
 
     #region UtilityMethods
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, 0f);
-        
+        // Todo: eventually fix zero and add jumpower based on button press length.
+        rb.velocity = new Vector2(rb.velocity.x, 0f);    
 
         if (isFloating)
         {
-            rb.AddForce(Vector2.up * jumpPower * floatingGravityScale, ForceMode2D.Impulse);
+            rb.AddForce(jumpPower * floatingGravityScale * Vector2.up, ForceMode2D.Impulse);
         }
         else
         {
-            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            rb.AddForce(jumpPower * defaultGravityScale * Vector2.up, ForceMode2D.Impulse);
         }
 
         currentJumps--;
